d187342f2e8017823008a4b422bb8f49edaf209f
Fazlay-Elahi-Rafi <fazlayrafi2001@gmail.com>
2026-04-02 04:01:00 +0600

    install react on Laravel

---
+++ app/View/backend/admin/dashboard.blade.php
@@ -0,0 +1,13 @@
+<x-app-layout>
+    <!--wrapper-->
+    <div class="wrapper">
+
+
+        <div class="page-wrapper">
+            {{-- @include('backend.admin.admin_dashboard') --}}
+        </div>
+
+    </div>
+    <!--end wrapper-->
+
+</x-app-layout>
